13b11960e41048fa637a9b696ebc57cfaccbbe8f
TAHAR Noureddine <71546633+EngrTAHAR-Noureddine@users.noreply.github.com>
2023-09-20 02:56:12 +0100

    fix

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import sys
 import time
+# import appscript
 
 
 def run_forum_linux(url, main_path, file_path):
-    command = 'cd ' + main_path + 'python3 ' + file_path + " " + url
+    command = 'cd ' + main_path + ' && python3 ' + file_path + " " + url #
+    # appscript.app('Terminal').do_script(command)
     os.system("gnome-terminal --window -- bash -c '" + command + "; exec bash'")
 
 
@@ -15,12 +17,12 @@ if __name__ == '__main__':
     if len(sys.argv) > 0:
         base_url_get_password = sys.argv[1]
         base_url_add_users = sys.argv[2]
-        while i < 1:
-            i = i + 1
+        while i < 20:
             try:
                 run_forum_linux(base_url_get_password, main_path, 'get_passwords.py')
                 run_forum_linux(base_url_add_users, main_path, 'add_users.py')
             except Exception as error:
                 print("error : ", error)
                 pass
-            time.sleep(1000)
+            i = i + 1
+            # time.sleep(500)
